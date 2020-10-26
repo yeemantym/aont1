054e6bd80b0e9ad263d2b06b2d39a9cfb492cf4b
yeeman <yee.tan@telecom-paristech.fr>
2020-10-26 18:50:41 +0100

    with inputstream

--- src/aont1/aont.java
+++ src/aont1/aont.java
@@ -4,6 +4,8 @@ package aont1;
 import aont1.CryptoUtils;
 import javax.crypto.SecretKey;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.nio.file.Files;
 
 
@@ -11,7 +13,7 @@ public class aont {
 	
 	public static void main(String[] args) throws Exception{
 		long startTime = System.nanoTime();
-		String filePath = "/home/yeeman/Documents/500MB.txt";
+		String filePath = "/home/yeeman/Documents/1MB.txt";
 		
 		// encrypt and decrypt need the same key.
         // get AES 256 bits (32 bytes) key
@@ -22,22 +24,35 @@ public class aont {
         byte[] iv = CryptoUtils.getRandomNonce(12);
         
 		File file = new File(filePath);
-		byte[] inputArray = Files.readAllBytes(file.toPath());
-		
-		//apply Bastion AONT to inputArray
-		int t = 0;
-		for (int i=0; i<inputArray.length; i++) {
-			t=t^inputArray[i];
+		//byte[] inputArray = Files.readAllBytes(file.toPath());
+		FileInputStream is = new FileInputStream(file);
+		FileOutputStream os = new FileOutputStream(new File("output_aont1.txt"));
+		byte[] inputArray = new byte[4096];
+		int read = 0;
+		while((read = is.read(inputArray)) > 0) {
+			//apply Bastion AONT to inputArray
+			int t = 0;
+			for (int i=0; i<inputArray.length; i++) {
+				t=t^inputArray[i];
+			}
+			
+			byte[] inputArrayAONT = new byte[inputArray.length]; 
+			for (int i=0; i<inputArray.length; i++) {
+				inputArrayAONT[i]=(byte)(inputArray[i]^t);
+			}
+			
+			//encrypt inputArrayAONT in parallel with aes 
+			byte[] encryptedArray = new byte[inputArray.length];
+			encryptedArray = aes_gcm.encryptWithPrefixIV(inputArrayAONT, secretKey, iv);
+		    //os.write(encryptedArray);
+		    for (int i=0; i<encryptedArray.length; i++) { 
+				os.write(encryptedArray[i]);		
+			}
 		}
-		
-		byte[] inputArrayAONT = new byte[inputArray.length]; 
-		for (int i=0; i<inputArray.length; i++) {
-			inputArrayAONT[i]=(byte)(inputArray[i]^t);
-		}
-		
-		//encrypt inputArrayAONT in parallel with aes 
-		byte[] encryptedArray = new byte[inputArray.length];
-		encryptedArray = aes_gcm.encryptWithPrefixIV(inputArrayAONT, secretKey, iv);
+		is.close();
+		os.close();
+
+	
 	/*	IntStream.range(0, inputArray.length).parallel().forEach(i->{
 			encryptedArray[i]=aes.encrypt(Integer.toString(inputArrayAONT[i]), secretKey);
 		});*/
@@ -55,7 +70,7 @@ public class aont {
 				System.out.println();
 			}		
 		}*/
-		System.out.println("Length of encryptedArray: "+encryptedArray.length);		
+		//System.out.println("Length of encryptedArray: "+encryptedArray.length);		
 		long endTime = System.nanoTime();
 		System.out.println("Took "+(endTime - startTime) + " ns"); 
 		
